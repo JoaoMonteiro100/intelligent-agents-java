d9804bd11b31e20a9056d0ee4c161835c059a8db
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-12-14 04:03:54 +0200

    Removed redundancy; added explanation

--- src/logic/RandomPlayer.java
+++ src/logic/RandomPlayer.java
@@ -5,14 +5,14 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomPlayer extends Player {
 
 	public int bid(int maxBid){
-		int coin = ThreadLocalRandom.current().nextInt(0,2);
-		if (maxBid+1 < 0.3*money && coin == 1){
-			return ThreadLocalRandom.current().nextInt(maxBid + 1, (int) (0.3 * money));
+		int coin = ThreadLocalRandom.current().nextInt(0,2); //he tosses a coin (heads or tails)
+		if (maxBid+1 < 0.3*money && coin == 1){ //if he has enough money and it was heads
+			return ThreadLocalRandom.current().nextInt(maxBid + 1, (int) (0.3 * money)); //he will bid some affordable amount
 		}
-		else if (coin == 1) {
-			return ThreadLocalRandom.current().nextInt(0, maxBid);
+		else if (coin == 1) { //if he doesn't have enough money and it was heads
+			return ThreadLocalRandom.current().nextInt(1, maxBid); //he will bid lower than the max bid
 		}
-		return 0;
+		return 0; //if he doesn't have enough money and was tails, he passes
 	}
 
 	@Override
