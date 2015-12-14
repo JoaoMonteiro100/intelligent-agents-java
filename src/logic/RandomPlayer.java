524ffae66318086a9e7632a08f689cd5d9897f94
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-12-14 03:57:59 +0200

    Increased Random's randomness
    He will now *sometimes* bid *something* even if it's not to outbid
    someone else

--- src/logic/RandomPlayer.java
+++ src/logic/RandomPlayer.java
@@ -5,10 +5,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomPlayer extends Player {
 
 	public int bid(int maxBid){
-		if (maxBid+1 < 0.3*money){
-			int coin = ThreadLocalRandom.current().nextInt(0,2);
-			if(coin == 1)
-				return ThreadLocalRandom.current().nextInt(maxBid + 1, (int) (0.3 * money));
+		int coin = ThreadLocalRandom.current().nextInt(0,2);
+		if (maxBid+1 < 0.3*money && coin == 1){
+			return ThreadLocalRandom.current().nextInt(maxBid + 1, (int) (0.3 * money));
+		}
+		else if (coin == 1) {
+			return ThreadLocalRandom.current().nextInt(0, maxBid);
 		}
 		return 0;
 	}
